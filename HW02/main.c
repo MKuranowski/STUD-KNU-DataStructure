f7f994d329c80750c90151bc5f6257f709afd6bd
Mikołaj Kuranowski <mkuranowski@gmail.com>
2022-10-10 20:00:35 +0900

    2nd homework: more tests

--- HW02/main.c
+++ HW02/main.c
@@ -4,17 +4,18 @@
 #include "stack_HW.h"
 
 char* tests[] = {
-    "324*+",
-    "24*3+",
-    "842-+",
-    "814*/",
+    "324*+",  // 11
+    "24*3+",  // 11
+    "842-+",  // 10
+    "814*/",  // 2
+    "468182++-*+", // -14
     NULL,
 };
 
 int main(void) {
     for (char** str = tests; *str; ++str) {
         double result = evalPostfix(*str);
-        printf("%s -> %f\n", *str, result);
+        printf("%s\t%f\n", *str, result);
     }
     return 0;
 }
