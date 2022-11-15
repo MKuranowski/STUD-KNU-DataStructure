3c883bf89e5db419a1eaaa039de47dc2e48193db
Mikołaj Kuranowski <mkuranowski@gmail.com>
2022-11-15 15:23:27 +0900

    HW04: add template

---
+++ HW04/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "generalList2.h"
+
+int sort_rule(Element point1, Element point2) {
+    // (1) fill this part
+}
+
+int main() {
+    Lhead* plist = createList();
+    // (2), (3), (4) fill this part
+}
